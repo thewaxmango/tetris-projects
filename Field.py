0a157830eab9c5c10a31dc6c31133f5178464dfd
thewaxmango <77765712+thewaxmango@users.noreply.github.com>
2023-05-12 07:25:37 -0400

    Update Field.py

--- Field.py
+++ Field.py
@@ -1,12 +1,48 @@
-from constants import *
-import Piece, Field
+import Field
+import py_fumen
 
-class Field:
-    def __init__(self, field:Field = None, width:int = BOARD_WIDTH, height:int = BOARD_HEIGHT, active:Piece = Piece()):
-        self.width, self.height = width, height
-        self.field = field
-        self.active = active
+class Replay_Frame:
+    def __init__(self) -> None:
+        self._field = None
+        self._message = None 
+        self._queue = None
+        self._garbage_queue = None
+        self._root_time = None
+        self._timestamp = None
+    
+    def set_field(self, field: Field.Field):
+        pass
 
-    def copy(self) -> Field:
-        return Field(field = self.field, width = self.width, height = self.height, active = self.active.copy())
+    def set_message(self, message: str):
+        pass
 
+    def set_queue(self, queue: Field.Queue):
+        pass
+
+    def set_garbage_queue(self, queue: list):
+        pass
+
+    def set_root_time(self, time: float):
+        pass
+    
+    def set_time(self, time: float):
+        pass
+
+    def to_py_fumen(self) -> py_fumen.page.Page:
+        pass
+
+class Replay:
+    def __init__(self) -> None:
+        self._frames = []
+
+    def add_frame(self, frame: Replay_Frame) -> None:
+        pass
+
+    def get_frame(self, index: int) -> Replay_Frame:
+        pass
+
+    def reset(self) -> None:
+        pass
+
+    def to_py_fumen(self) -> list:
+        pass 
