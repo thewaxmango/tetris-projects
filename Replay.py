9ca99eaaf7d92b52b823a9edd1ce1e3ab5bdb942
thewaxmango <77765712+thewaxmango@users.noreply.github.com>
2023-05-12 07:26:41 -0400

    Update Replay.py

--- Replay.py
+++ Replay.py
@@ -1,11 +1,48 @@
-class Replay:
-    def __init__(self) -> None:
-        self.frames = []
+import Field
+import py_fumen
 
 class Replay_Frame:
     def __init__(self) -> None:
-        self.field = None
-        self.message = None 
-        self.queue = None
-        self.garbage_queue = None
-        self.timestamp = 0
+        self._field = None
+        self._message = None 
+        self._queue = None
+        self._garbage_queue = None
+        self._root_time = None
+        self._timestamp = None
+    
+    def set_field(self, field: Field.Field):
+        pass
+
+    def set_message(self, message: str):
+        pass
+
+    def set_queue(self, queue: Field.Queue):
+        pass
+
+    def set_garbage_queue(self, queue: list):
+        pass
+
+    def set_root_time(self, time: float):
+        pass
+    
+    def set_time(self, time: float):
+        pass
+
+    def to_py_fumen(self) -> py_fumen.page.Page:
+        pass
+
+class Replay:
+    def __init__(self) -> None:
+        self._frames = []
+
+    def add_frame(self, frame: Replay_Frame) -> None:
+        pass
+
+    def get_frame(self, index: int) -> Replay_Frame:
+        pass
+
+    def reset(self) -> None:
+        pass
+
+    def to_py_fumen(self) -> list:
+        pass 
